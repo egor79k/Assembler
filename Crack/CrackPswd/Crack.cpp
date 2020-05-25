99c30b2a0ee824c3b18c1161a6cdb1acc0ada30a
egor79k <egor79k@yandex.ru>
2020-05-25 09:36:19 +0300

    Auto commiting

--- Crack/CrackPswd/Crack.cpp
+++ Crack/CrackPswd/Crack.cpp
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
-#define end1 	printf("\x1b[31m\n\tError: wrong file!\n\x1b[30m\n\tPress F to end program...\n");	\
-				Wait																			\
+#define END1 	printf("\x1b[31m\n\tError: wrong file!\n\x1b[30m\n\tPress F to end program...\n");	\
+				WAIT																			\
 				printf("\x1b[0m\e[1;1H\e[2J\n");													\
 				return 0;						 													\
 
-#define Circle(x) 	printf (x);				\
+#define CIRCLE(x) 	printf (x);				\
 					fflush (stdout);		\
 					usleep (500000);		\
 
-#define Wait	printf("\n\tPress any key to continue...\n");	\
+#define WAIT	printf("\n\tPress any key to continue...\n");	\
 				getchar ();										\
 
-#define Duck	"\U0001F986"
+#define DUCK	"\U0001F986"
 
-#define Complete printf("\x1b[31m\n\tComplete! %s\n\x1b[30m", Duck);
+#define COMPLETE printf("\x1b[31m\n\tComplete! %s\n\x1b[30m", DUCK);
 
 
 const char crack_1[] = "//==\\\\  ||=\\\\     //\\\\     //==\\\\  || //        //==\\\\    //==\\\\";
@@ -31,11 +31,11 @@ const short new_jump    = 0x43eb;
 
 void ProgressCircle ()
 {
-	Circle ("\U000025CB");
-	Circle ("\b\U000025D4");
-	Circle ("\b\U000025D1");
-	Circle ("\b\U000025D5");
-	Circle ("\b\U000025Cf");
+	CIRCLE ("\U000025CB");
+	CIRCLE ("\b\U000025D4");
+	CIRCLE ("\b\U000025D1");
+	CIRCLE ("\b\U000025D5");
+	CIRCLE ("\b\U000025Cf");
 	return;
 }
 
@@ -44,7 +44,7 @@ void ProgressBar ()
 {
 	for (int i = 0; i <= 100; i += 2)
 	{
-		printf("%s%2d%% %s", u8"\U00002588", i, Duck);
+		printf("%s%2d%% %s", u8"\U00002588", i, DUCK);
 		usleep(200000);
 		fflush(stdout);
 		printf("\b\b\b\b\b\b");
@@ -57,8 +57,8 @@ int main ()
 {
 	printf("\x1b[1;31;47m\e[1;1H\e[2J\n");
 	printf("\t\t\t%s\n\t\t\t%s\n\t\t\t%s\n\t\t\t%s\n", crack_1, crack_2, crack_3, crack_4);
-	printf("\n\n\tWelcome to the Crack 2.0 %s\n\x1b[30m", Duck);
-	Wait
+	printf("\n\n\tWelcome to the Crack 2.0 %s\n\x1b[30m", DUCK);
+	WAIT
 
 	char buffer[file_size] = {};
 	int hash_summ = 0;
@@ -71,11 +71,11 @@ int main ()
 	fseek (executable, 0, SEEK_END);							//Check file size
 	if (ftell (executable) != file_size)
 	{
-		end1
+		END1
 	}
 
-	Complete
-	Wait
+	COMPLETE
+	WAIT
 
 
 	printf("\n\tFile hash checking ");
@@ -92,21 +92,21 @@ int main ()
 
 	if (hash_summ != file_hash)
 	{
-		end1
+		END1
 	}
 
-	Complete
-	Wait
+	COMPLETE
+	WAIT
 
-	printf ("\t%s: Are you ready???", Duck);
+	printf ("\t%s: Are you ready???", DUCK);
 	getchar ();
 	printf ("\tYou: \U0001F425 Yes!\n");
 	usleep (800000);
-	printf ("\t%s: I can't hear you?!", Duck);
+	printf ("\t%s: I can't hear you?!", DUCK);
 	getchar ();
 	printf ("\tYou: \U0001F425 Yes!!!\n");
 	usleep (800000);
-	printf ("\t%s: Ok. Take your hands away from the keyboard! Professional is working!\n\n\t", Duck);
+	printf ("\t%s: Ok. Take your hands away from the keyboard! Professional is working!\n\n\t", DUCK);
 	usleep (800000);
 	ProgressBar ();
 	printf("\n");
@@ -115,9 +115,9 @@ int main ()
 
 	fclose (executable);
 
-	Complete
+	COMPLETE
 
-	Wait
+	WAIT
 
 	printf("\x1b[0m\e[1;1H\e[2J\n");
 	return 0;
